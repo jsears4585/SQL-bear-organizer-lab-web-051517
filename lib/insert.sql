4c63b5126e8c12e7eb758ad598f9906f3bbce97b
Jason Sears <jsears4585@gmail.com>
2017-05-21 17:10:00 -0400

    Added bears to db.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 20, "M", "Brown", "Ornery", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 8, "M", "Brown", "Salmon Addict", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "F", "Brown", "Pround Mama", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 22, "M", "Brown", "Fun-loving", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 17, "F", "Brown", "Happy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 2, "M", "Brown", "Chill", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 6, "F", "Brown", "Ornery", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 23, "M", "Black", "Lethal", 0);
